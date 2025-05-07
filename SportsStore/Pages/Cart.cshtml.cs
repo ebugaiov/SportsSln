1bc70ec51a4af93acd2a80710cf7bdce7f8127f7
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-05-07 14:27:59 +0200

    add cart(not completed)

---
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+
+namespace SportsStore.Pages;
+
+public class CartModel(IStoreRepository repository) : PageModel
+{
+    private IStoreRepository _repository = repository;
+    
+    public Cart? Cart { get; set; }
+    public string ReturnUrl { get; set; } = "/";
+
+    public void OnGet(string returnUrl)
+    {
+        ReturnUrl = returnUrl ?? "/";
+        Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+    }
+
+    public IActionResult OnPost(long productId, string returnUrl)
+    {
+        Product? product = _repository.Products
+            .FirstOrDefault(p => p.ProductID == productId);
+        if (product != null)
+        {
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddItem(product, 1);
+            HttpContext.Session.SetJson("cart", Cart);
+        }
+
+        return RedirectToPage(new { returnUrl = returnUrl });
+    }
+}
